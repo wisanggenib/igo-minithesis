9a374910f28e07375e049dd7ed9f7b59ec3dc1e2
Bagus Wisanggeni <bagus.wisanggeni040897@gmail.com>
2020-09-15 16:14:42 +0700

    testing API for Ongkir

---
+++ testingapi.php
@@ -0,0 +1,32 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.rajaongkir.com/starter/province",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "key: 5d1ad4b0bd78e1d11fa8037460f6b18f"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+}
+$data = json_decode($response, true);
+for ($i=0;$i < count($data['rajaongkir']['results']);$i++) {
+    echo ($data['rajaongkir']['results'][$i]['province'])."<br>";
+}
+
+
+?>
