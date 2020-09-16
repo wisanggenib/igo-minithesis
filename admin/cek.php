a1f0363e5d94774d5e2311dac7ed301425efa36c
Bagus Wisanggeni <bagus.wisanggeni040897@gmail.com>
2020-09-16 11:07:12 +0700

    Template For Laporan

--- admin/cek.php
+++ admin/cek.php
@@ -1,14 +1,43 @@
 <?php
 // menyertakan autoloader
 require_once 'dompdf/autoload.inc.php';
-
+include "../lib/koneksi.php";
+$jumlah = 0;
 // mengacu ke namespace DOMPDF
 use Dompdf\Dompdf;
 
 // menggunakan class dompdf
 $dompdf = new Dompdf();
 
-$html = file_get_contents("index1.html");
+$html="<center>
+<h3>Laporan Harian</h3>
+<table align='center'>
+    <tr>
+        <th>Id Transaksi</th>
+        <th>Nama Pelanggan</th>
+        <th>Tanggal Pembelian</th>
+        <th>Total Harga</th>
+    </tr>";
+    $kueripembayaran = mysqli_query($host, "SELECT * FROM transaksi WHERE status = 'complete' ");
+    while ($valPem = mysqli_fetch_array($kueripembayaran, MYSQLI_ASSOC)) {
+    $kueriproduk = mysqli_query($host, "SELECT detail_transaksi.harga_produk,detail_transaksi.qty from detail_transaksi JOIN produk ON detail_transaksi.id_produk = produk.id_produk WHERE id_transaksi = '$valPem[id_transaksi]'");
+    while ($val = mysqli_fetch_array($kueriproduk, MYSQLI_ASSOC)) {
+        $jumlah=$jumlah + ($val['harga_produk'] * $val['qty']);
+    }
+        $html .="
+        <tr>
+            <td>".$valPem['id_transaksi']."</td>
+            <td>".$valPem['nama_penerima']."</td>
+            <td>".$valPem['tgl_pesan']."</td>
+            <td>".$jumlah."</td>
+        </tr>
+        ";
+}
+$html .="</table>
+</center>
+</body>
+</html>";
+
 $dompdf->loadHtml($html);
 
 // (Opsional) Mengatur ukuran kertas dan orientasi kertas
