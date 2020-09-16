3357c478b77bb924864e958d5d6b8ab0313ef974
Bagus Wisanggeni <bagus.wisanggeni040897@gmail.com>
2020-09-16 23:35:26 +0700

    button cetak laporan fix

--- admin/pages/pembayaran/pembayaran.php
+++ admin/pages/pembayaran/pembayaran.php
@@ -21,21 +21,14 @@
                                         <h3 class="title-5 m-b-35">List Penjualan</h3>
                                         <div class="table-data__tool">
                                             <div class="table-data__tool-left">
-                                                <div class="rs-select2--light rs-select2--sm">
-                                                    <select class="js-select2" name="time">
-                                                        <option selected="selected">Today</option>
-                                                        <option value="">3 Days</option>
-                                                        <option value="">1 Week</option>
-                                                    </select>
-                                                    <div class="dropDownSelect2"></div>
-                                                </div>
+                                                
                                             </div>
                                             <div class="table-data__tool-right">
                                                 <div class="rs-select2--dark rs-select2--sm rs-select2--dark2">
-                                                    <select class="js-select2" name="type">
-                                                        <option selected="selected">Export</option>
-                                                        <option value="">Option 1</option>
-                                                        <option value="">Option 2</option>
+                                                    <select class="js-select2" name="type" onchange="location = this.value;">
+                                                        <option value="#" selected="selected">Export</option>
+                                                        <option value="laporan_bulanan.php">Bulan</option>
+                                                        <option value="laporan_tahunan.php">Tahun</option>
                                                     </select>
                                                     <div class="dropDownSelect2"></div>
                                                 </div>
